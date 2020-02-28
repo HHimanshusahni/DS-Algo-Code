71e28f56b47c284d4e5b382285837e37ea31034b
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-02-28 08:27:36 +0530

    Finding Number of Digits in a number recursively

---
+++ coding ninjas practice code/8 Recursion 1/NumberOfDigits.java
@@ -0,0 +1,10 @@
+public class NumberOfDigits{
+	public static int count(int n){
+		if(n==0)
+			return 0 ;
+		return 1+count(n/10);
+	}
+	public static void main(String[] args){
+		System.out.print(count(156));
+	}
+}
