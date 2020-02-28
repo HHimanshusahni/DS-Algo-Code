3ba3d0f54b48c78c35228a14cc3fcb7c6b53c345
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-02-28 18:47:25 +0530

    Optimized power of variable solution

--- coding ninjas practice code/8 Recursion 1/Power.java
+++ coding ninjas practice code/8 Recursion 1/Power.java
@@ -1,10 +1,40 @@
 public class Power{
+
+	//O(n)  Recurrence relation : T(n) = T(n-1) + 1
 	public static int power(int x, int n){
 		if(n==0)
 			return 1;
 		return x * power(x,n-1);
 	}
+
+
+	//Divide and conquer
+	// O(n) Recurrence Relation : T(n)  = 2T(n/2) + 1
+	public static int power_2(int x, int n){
+		// base case
+		if(n==0)
+			return 1;
+		if(n%2==0)
+			return power_2(x,n/2) * power_2(x,n/2); // recursive call
+		else 
+			return x * power_2(x,n/2) * power_2(x,n/2);
+
+	}
+
+
+	// 0(log n)   Recurrence Relation : T(n)  = T(n/2) + 1
+	public static int power_3(int x , int n ){
+		if(n==0)
+			return 1;
+		int smallAns = power_3(x,n/2); // recursive call
+
+		if(n%2==0)
+			return smallAns * smallAns;
+		else 
+			return x * smallAns * smallAns;
+	}
+	
 	public static void main(String[] args){
-		System.out.println(power(3,4));
+		System.out.println(power_3(3,4));
 	}
 }
