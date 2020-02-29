f5f34401557b6335ce1c74572941330039970fce
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-02-29 12:19:50 +0530

    Counting no of zeroes in the numberrecursively

---
+++ coding ninjas practice code/8 Recursion 1/CountZeros.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class CountZeros{
+	public static int countZerosRec(int input){
+		if(input < 10){
+			if(input==0)
+				return 0;
+		}
+		int smallAns = countZerosRec(input/10);
+		return input%10 == 0 ? smallAns+1 : smallAns;
+	}
+
+	public static void main (String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		System.out.println(countZerosRec(num));
+	}
+}
