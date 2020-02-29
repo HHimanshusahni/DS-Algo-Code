caa6ce1ac7052c2fe6c180a6e78d2d0f505e6ff8
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-02-29 11:15:07 +0530

    Approach 2 for finding all the indices of array of number recursively

--- coding ninjas practice code/8 Recursion 1/AllIndicesOfANumber.java
+++ coding ninjas practice code/8 Recursion 1/AllIndicesOfANumber.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class AllIndicesOfANumber{
-
+	//Approach 1 //Time Complexity : 0(n)  as T(n) = T(n-1)+1
 	public static int[] allIndexes(int input[],int x){
 		int arr[] = new int[1000];
 		int solIndex = allIndexes(input,arr,0,0,x);
@@ -19,6 +19,30 @@ public class AllIndicesOfANumber{
 		return allIndexes(input,arr,index+1,solIndex,x);
 	}
 
+	// Approach 2 Recurrence Relation : T(n) = T(n-1)+n Time complexity = 0(n^2)
+	public static int[] allIndexes_2(int input[],int x){
+		return allIndexes_2(input,x,0);
+	}
+	public static int[] allIndexes_2(int input[],int x,int index){
+		//base case
+		if(index ==input.length){
+			int output[] = new int[0];
+			return output;
+		}
+		// recursive step
+		int [] smallAns = allIndexes_2(input,x,index+1);
+
+		//our work
+		if(input[index] == x){
+			int ans [] = new int[smallAns.length+1];
+			ans[0] = index;
+			for(int i =0;i<smallAns.length;i++){
+				ans[i+1] = smallAns[i];
+			}
+			return ans;
+		}
+		return smallAns;
+	}
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
@@ -27,7 +51,7 @@ public class AllIndicesOfANumber{
 		for(int i =0;i<n;i++)
 			input[i] = Integer.parseInt(arr[i]);
 		int x = Integer.parseInt(br.readLine());
-		int ansArr [] = allIndexes(input,x);
+		int ansArr [] = allIndexes_2(input,x);
 
 		for(int ans:ansArr)
 			System.out.print(ans+" ");
