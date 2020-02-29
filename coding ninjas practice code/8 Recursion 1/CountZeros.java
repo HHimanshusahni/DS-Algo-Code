bccd3d1536b7d25875b749bc19f0212e7e39ec45
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-02-29 12:21:07 +0530

    Handled the case in counting zeros when only 0 is passed as input

--- coding ninjas practice code/8 Recursion 1/CountZeros.java
+++ coding ninjas practice code/8 Recursion 1/CountZeros.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 public class CountZeros{
 	public static int countZerosRec(int input){
 		if(input < 10){
-			if(input==0)
-				return 0;
+			if(input == 0)
+				return 1;
+			else 
+				return 0 ;
 		}
 		int smallAns = countZerosRec(input/10);
 		return input%10 == 0 ? smallAns+1 : smallAns;
