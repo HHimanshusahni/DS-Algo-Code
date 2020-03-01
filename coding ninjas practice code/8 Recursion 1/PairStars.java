ceb440c9a1ec4affc7d4f40df4bf7eb58c300c60
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-03-01 09:04:23 +0530

    Pair Stars Approach2

--- coding ninjas practice code/8 Recursion 1/PairStars.java
+++ coding ninjas practice code/8 Recursion 1/PairStars.java
@@ -12,8 +12,21 @@ public class PairStars{
 		// our calculation
 		return s.charAt(0) == smallAns.charAt(0) ? s.charAt(0)+"*"+smallAns : s.charAt(0)+smallAns; 
 	}	
+
+	// Approch 2  hello ---> hel*l| h
+	public static String addStars_2(String s){
+		//base case
+		if(s.length()==1)
+			return s;
+		//recursive step
+		String smallAns = addStars_2(s.substring(0,s.length()-1));
+		// calculation
+		char current= s.charAt(s.length()-1);
+		char prev = smallAns.charAt(smallAns.length()-1);
+		return prev == current ? smallAns+"*"+current : smallAns+current;
+	}
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(addStars(br.readLine()));
+		System.out.println(addStars_2(br.readLine()));
 	}
 }
