8b8b8f3251d6dbd50bf2690352de8061db3e2f26
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-03-01 08:37:13 +0530

    Improving base condition in ReplacePi

--- coding ninjas practice code/8 Recursion 1/ReplacePi.java
+++ coding ninjas practice code/8 Recursion 1/ReplacePi.java
@@ -1,11 +1,8 @@
 import java.io.*;
 public class ReplacePi{
 	public static String replace(String input){
-		if(input.length() == 0  )
-			return "";
-		if(input.length() ==1)
-			return input.charAt(0)+"";
-
+		if(input.length() <=1 )
+			return input;
 		if(input.charAt(0) == 'p' && input.charAt(1) == 'i')
 			return "3.14"+replace(input.substring(2));
 		
