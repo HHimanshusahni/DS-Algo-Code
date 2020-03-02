b861c69c6a414d9db3f0738099bf18160475cdb6
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-03-02 08:44:22 +0530

    Removing Duplicates approach 2

--- coding ninjas practice code/9 Recursion 2/RemoveDuplicates.java
+++ coding ninjas practice code/9 Recursion 2/RemoveDuplicates.java
@@ -13,8 +13,20 @@ public class RemoveDuplicates{
 		char current = s.charAt(s.length()-1);
 		return prev == current ? smallAns :smallAns+current;
 	}
+
+	// approach 2 s|tring
+	public  static String removeConsecutiveDuplicates_2(String s){
+		// base case
+		if(s.length()<=1)
+			return s;
+		//Recursive  case
+		String smallAns = removeConsecutiveDuplicates_2(s.substring(1));
+
+		// Small Calculation
+		return smallAns.charAt(0) == s.charAt(0) ? smallAns : s.charAt(0)+smallAns; 
+	}
 	public static void main(String [] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(removeConsecutiveDuplicates(br.readLine()));
+		System.out.println(removeConsecutiveDuplicates_2(br.readLine()));
 	}
 }
