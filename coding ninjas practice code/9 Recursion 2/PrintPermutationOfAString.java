5f29bde71131403dc0469cd74004592e1bcbd538
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-05 20:27:06 +0530

    Approch 2 of Prining permutation of a string using backtracking

--- coding ninjas practice code/9 Recursion 2/PrintPermutationOfAString.java
+++ coding ninjas practice code/9 Recursion 2/PrintPermutationOfAString.java
@@ -18,6 +18,33 @@ public class PrintPermutationOfAString{
 			permutations(input.substring(0,i)+input.substring(i+1),outputSoFar + ch);
 		}
 	}
+
+	// https://www.youtube.com/watch?v=GuTPwotSdYw
+	// Approach 2
+	public static void permutations_2(String input){
+		char arr[] = input.toCharArray();
+		permutations_2(arr,0,input.length()-1);
+	}
+	public static void permutations_2(char arr[],int l , int r){
+		// Base Case
+		if(l == r)
+			System.out.println(String.valueOf(arr));
+		else{
+			// Recursive step
+			for(int i = l;i<=r;i++){
+				swap(arr,l,i);
+				permutations_2(arr,l+1,r);
+				swap(arr,l,i);// backtrack
+
+			}
+		}
+	}
+	private static void swap(char arr[],int index1 ,int index2){
+		char temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		permutations(br.readLine());
