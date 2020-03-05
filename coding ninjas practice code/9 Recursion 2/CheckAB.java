b77b3bbfd988d79ec532c826b935168df82a2cd4
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-05 10:34:06 +0530

    Checking string formed from a and b following set of rules

---
+++ coding ninjas practice code/9 Recursion 2/CheckAB.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class CheckAB{
+	public static boolean checkAB(String input){
+		// Base case
+		if(input.length() == 0)
+			return true;
+		// Recursive case and small caluculation
+		if(input.charAt(0)=='a'){
+			if(input.length()>=3 && input.charAt(1) =='b'){
+				if(input.charAt(2) == 'b'){
+					return checkAB(input.substring(3));
+				}
+				return false;
+			}else
+				return checkAB(input.substring(1));
+
+		}else
+			return false;
+	}
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(checkAB(br.readLine()));
+	}
+}
