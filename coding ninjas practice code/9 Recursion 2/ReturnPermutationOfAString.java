dce2b48e20ed1344d7f9109268b89fcdeeabf807
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-05 14:09:51 +0530

    Approach 2 Return permutation of a string recursively

--- coding ninjas practice code/9 Recursion 2/ReturnPermutationOfAString.java
+++ coding ninjas practice code/9 Recursion 2/ReturnPermutationOfAString.java
@@ -28,9 +28,36 @@ public class ReturnPermutationOfAString{
 
 		return ans;
 	}
+// Approach 2
+	public static String[] permutationOfString_2(String input){
+		//base case
+		if(input.length() == 0){
+			String output[] = new String[0];
+			return output;
+		}
+		if(input.length() == 1){
+			String output[] = {input};
+			return output;
+		}
+		// Recursive case
+		int fact = 1;
+		for(int i = 1;i<=input.length();i++)
+				fact = i*fact;
+		int counter = 0 ;
+		String ans[] = new String[fact];
+		for(int i =0;i<input.length();i++){
+			String smallAns[] = permutationOfString_2(input.substring(0,i)+input.substring(i+1));
+			for(int j = 0;j<smallAns.length;j++){
+				ans[counter++] = input.charAt(i)+ smallAns[j];
+			}
+
+		}
+		return ans;
+
+	}
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String ans [] = permutationOfString(br.readLine());
+		String ans [] = permutationOfString_2(br.readLine());
 
 		for(String str : ans)
 			System.out.println(str);
