ccdf77537863e0eaeabd4df03741e14f429b7669
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-03-12 20:53:36 +0530

    Better way of coding Complex Numbers

--- coding ninjas practice code/11 OOPS/ComplexNumber.java
+++ coding ninjas practice code/11 OOPS/ComplexNumber.java
@@ -1,41 +1,41 @@
 import java.io.*;
-public class ComplexNumber{
+ public class ComplexNumber {
 	int R;
 	int I;
 	ComplexNumber(int R, int I){
 		 this.R = R;
 	     this.I = I;
 	}
-	public static void plus(ComplexNumber c1 ,ComplexNumber c2){
-		c1.R = c1.R + c2.R;
-		c1.I = c1.I + c2.I;
+	public void plus(ComplexNumber c2){
+		this.R = this.R + c2.R;
+		this.I = this.I + c2.I;
 
 	}
-	public static void multiply(ComplexNumber c1 ,ComplexNumber c2){
-		int r = (c1.R * c2.R)-(c1.I * c2.I);
-		int i = (c1.I * c2.R) +(c1.R * c2.I);
-		c1.R = r;
-		c1.I = i;
+	public  void multiply(ComplexNumber c2){
+		int r = (this.R * c2.R)- (this.I * c2.I);
+		int i = (this.I * c2.R) +(this.R * c2.I);
+		this.R = r;
+		this.I = i;
 	}
-	public static void print(ComplexNumber c1){
-		System.out.println(c1.R+" + i"+c1.I);
+	public void print(){
+		System.out.println(this.R+" + i"+this.I);
 	}
 	public static void main(String[]args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String strArr[] = br.readLine().split(" ");
-		int r =Integer.parseInt(strArr[0]);
+		int r = Integer.parseInt(strArr[0]);
 		int i = Integer.parseInt(strArr[1]);
 		ComplexNumber c1 = new ComplexNumber(r,i);
 		strArr = br.readLine().split(" ");
-		r =Integer.parseInt(strArr[0]);
+		r = Integer.parseInt(strArr[0]);
 	    i = Integer.parseInt(strArr[1]);
 		ComplexNumber c2 = new ComplexNumber(r,i);
 		int choice = Integer.parseInt(br.readLine());
 		if(choice == 1)
-			plus(c1,c2);
+			c1.plus(c2);
 		else if(choice == 2)
-			multiply(c1,c2);
-		print(c1);
+			c2.multiply(c2);
+		c1.print();
 
 	}
 }
