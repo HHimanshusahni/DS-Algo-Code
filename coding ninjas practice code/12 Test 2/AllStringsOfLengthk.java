74cb617b5e056a73f8650081a1980c53898ac813
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-17 12:36:27 +0530

    Finding all Strings of length k recursively time O(l^k)

---
+++ coding ninjas practice code/12 Test 2/AllStringsOfLengthk.java
@@ -0,0 +1,39 @@
+import java.io.*;
+public class AllStringsOfLengthk {
+	
+	public static String[] allStrings(String charSet, int len){
+		//Base Case
+		if(len == 0){
+			 String[] strArr = new String[0];
+			 return strArr;
+		}
+		else if(len == 1){
+			 String[] strArr = new String[charSet.length()];
+			 for(int i = 0;i<charSet.length();i++)
+			 	strArr[i] =  charSet.charAt(i)+"";
+			 return strArr;
+		}
+
+		//Recursive Case
+		String[] smallAns = allStrings(charSet,len-1);
+
+		//Small Calculation
+		String[] ans = new String[smallAns.length * charSet.length()]; 
+		for(int i = 0 ,counter = 0;i<charSet.length();i++)
+			for(int j = 0 ;j<smallAns.length;j++)
+				ans[counter++] = charSet.charAt(i)+smallAns[j];
+			
+		return ans;
+	}
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int len = Integer.parseInt(br.readLine());
+		String ans[] = allStrings(s,len);
+		for(String a : ans){
+			System.out.println(a);
+		}
+	}
+
+}
