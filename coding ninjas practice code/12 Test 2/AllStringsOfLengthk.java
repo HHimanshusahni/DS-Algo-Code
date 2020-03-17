f1ed01bf8340804ea78f859f3691e14d96288ece
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-17 12:38:59 +0530

    Better way to code base case

--- coding ninjas practice code/12 Test 2/AllStringsOfLengthk.java
+++ coding ninjas practice code/12 Test 2/AllStringsOfLengthk.java
@@ -2,18 +2,13 @@ import java.io.*;
 public class AllStringsOfLengthk {
 	
 	public static String[] allStrings(String charSet, int len){
+		lic static String[] allStrings(String charSet, int len){
 		//Base Case
 		if(len == 0){
-			 String[] strArr = new String[0];
+			 String[] strArr = new String[1];
+             strArr[0] = "";
 			 return strArr;
 		}
-		else if(len == 1){
-			 String[] strArr = new String[charSet.length()];
-			 for(int i = 0;i<charSet.length();i++)
-			 	strArr[i] =  charSet.charAt(i)+"";
-			 return strArr;
-		}
-
 		//Recursive Case
 		String[] smallAns = allStrings(charSet,len-1);
 
