29725d86c0f08e693f2ef801e4e8c0912935c3b0
Himanshu Sahni <sahnihimanshu97@gmail.com>
2020-03-17 10:03:46 +0530

    Better solution with O(1) Space complexity

--- coding ninjas practice code/12 Test 2/SplitArray.java
+++ coding ninjas practice code/12 Test 2/SplitArray.java
@@ -38,6 +38,32 @@ public class SplitArray{
 		   	   splitArray(sum3,sum5,totalSum,list,i+1,subsetSum);
 
 	}
+
+
+	//Approach 2 Time Complexity = 0(2^n) and space complexity O(1)ie constant space
+	public static boolean splitArray2(int input[]){
+		return splitArray2(input,0,0,0);
+	}
+	public static boolean splitArray2(int input[],int index,int sum3,int sum5){
+		//Base Case
+		if(index == input.length){
+			if(sum3 == sum5)
+				return true;
+			else 
+				return false;
+		}
+		if(input[index]%5 == 0 ){
+			sum5 += input[index];
+			return splitArray2(input,index+1,sum3,sum5);
+		}else if(input[index]%3 == 0){
+			sum5  += input[index];
+			return splitArray2(input,index+1,sum3,sum5);
+		}else
+			return splitArray2(input,index+1,sum3+input[index],sum5)|| splitArray2(input,index+1,sum3,sum5+input[index]);
+		
+
+	}
+
 	public static void main(String[]args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
@@ -45,6 +71,6 @@ public class SplitArray{
 		String strArr[] = br.readLine().split(" ");
 		for(int i = 0 ;i<N;i++)
 			input[i] = Integer.parseInt(strArr[i]);
-	 	System.out.println(splitArray(input));
+	 	System.out.println(splitArray2(input));
 	}
 }
