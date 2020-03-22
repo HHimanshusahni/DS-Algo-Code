6dc222be2522a546fe657820afce4adcb7e60fcc
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-22 15:26:35 +0530

    BubbleSort on linked list swapping  the nodes not the data

--- coding ninjas practice code/14 LinkedList - 2/_05BubbleSortLinkedList.java
+++ coding ninjas practice code/14 LinkedList - 2/_05BubbleSortLinkedList.java
@@ -9,7 +9,7 @@ class LinkedListNode<T>{
 public class _05BubbleSortLinkedList{
 	public static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head){
 
-		LinkedListNode<Integer> t = head,t1,t2;
+		LinkedListNode<Integer> t = head,t1,t2,prev;
 		int len = 0;
 		while(t != null){
 			++len;
@@ -18,15 +18,32 @@ public class _05BubbleSortLinkedList{
 		for(int i = 0;i<len-1;i++){
 			t1 = head;
 			t2 = head.next;
+			prev = null;
 			for(int j = 0;j<len-i-1;j++){
 				if(t1.data.intValue() > t2.data.intValue()){
 					//swap the data of the nodes
-					Integer temp = t1.data;
-					t1.data = t2.data;
-					t2.data = temp;
-				}
-				t1 = t1.next;
-				t2 = t2.next;
+					// Integer temp = t1.data;
+					// t1.data = t2.data;
+					// t2.data = temp;
+
+					// Or Swapping the nodes and not the data
+					if(prev == null)
+						head = t2;
+					else
+						prev.next = t2;
+
+					t1.next = t2.next;
+					t2.next = t1;
+					
+					prev = t2;
+					t2 = t1.next;
+					
+
+				}else{
+					prev = t1;
+					t1 = t1.next;
+					t2 = t2.next;
+		   		}
 			}
 		}
 		return head;
