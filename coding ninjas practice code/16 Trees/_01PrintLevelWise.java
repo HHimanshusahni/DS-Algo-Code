0c6fb49ccf388d0ef0d9025c815fdee4fe7fd53e
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-27 14:17:13 +0530

    PrintLevelWise without using the flag

--- coding ninjas practice code/16 Trees/_01PrintLevelWise.java
+++ coding ninjas practice code/16 Trees/_01PrintLevelWise.java
@@ -5,16 +5,13 @@ class _01PrintLevelWise{
 		if(root == null)
 			return;
 		Queue<TreeNode<Integer>> q = new LinkedList<>();
-		System.out.println(root.data);
 		q.offer(root);
 		q.offer(null);
-		boolean flag = true;// flag true means that we have last element in the queue as null
 		while(!q.isEmpty()){
 			TreeNode<Integer> treeNode = q.poll();
 			if(treeNode == null){ // we have printed all the nodes of the level
-				if(flag)
+				if(q.isEmpty())// If no other element is present then that means it is the last element
 					break;
-				flag = true;
 				System.out.println();
 				q.offer(null); // inserting null in the list to mark the ending of one level
 			}else{
@@ -22,12 +19,8 @@ class _01PrintLevelWise{
 				//Add all the children to the queue
 				for(int i =  0;i<treeNode.children.size();i++)
 			  		q.offer(treeNode.children.get(i));
-			  	flag = false;
 			}
-			
 		}
-
-
 	}
 	public static void main(String[]args){
 		TreeUse tree = new TreeUse();
