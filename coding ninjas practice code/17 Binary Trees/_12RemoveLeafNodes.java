c53424ed3891b5affaa83c45163a3b2539410e88
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-30 18:52:47 +0530

    Approach1 remove leaf nodes

---
+++ coding ninjas practice code/17 Binary Trees/_12RemoveLeafNodes.java
@@ -0,0 +1,33 @@
+public class _12RemoveLeafNodes{
+
+	//Approach 1 --> Deleting from the parent of leaf node 
+	public static BinaryTreeNode<Integer> removeAllLeaves(BinaryTreeNode<Integer> root){
+		removeAllLeavesHelp(root);
+		return root;
+	}
+	public static void removeAllLeavesHelp(BinaryTreeNode<Integer>root){
+		//Base Case
+		if(root == null)
+			return  ;
+		//Small Calculation
+		if(root.left != null && root.left.left == null && root.left.right == null)
+			root.left = null;
+		if(root.right != null && root.right.left == null && root.right.right == null)
+			root.right = null;
+
+		//Recusive Case
+		if(root.left != null)
+			removeAllLeaves(root.left);
+		if(root.right != null)
+			removeAllLeaves(root.right);
+		
+	}
+	public static void main(String[]args){
+		BinaryTreeUse tree = new BinaryTreeUse();
+		BinaryTreeNode<Integer>root  = tree.takeInputLevelWise();
+		root = 	removeAllLeaves(root);
+		_08PrintLevelWise f = new _08PrintLevelWise();
+		f.printLevelWise(root);
+
+	}
+}
