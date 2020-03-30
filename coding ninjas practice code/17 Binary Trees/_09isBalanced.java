682d62143d97bbf87a3f9e5ca79f58fe19101ea2
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-30 15:14:01 +0530

    Approach 2 O(n)

--- coding ninjas practice code/17 Binary Trees/_09isBalanced.java
+++ coding ninjas practice code/17 Binary Trees/_09isBalanced.java
@@ -1,5 +1,9 @@
+class Pair{
+	boolean isbalanced;
+	int height;
+}
 public class _09isBalanced{
-	//Approch 1 Check Balanced
+	//Approch 1 Check Balanced  
 	public static boolean checkBalanced(BinaryTreeNode<Integer> root){
 		//Base Case
 		// if(root == null)
@@ -19,9 +23,41 @@ public class _09isBalanced{
 	public static int depthOfTree(BinaryTreeNode<Integer> root){
 		return root == null ? 0 : 1 +Math.max(depthOfTree(root.left),depthOfTree(root.right));
 	}
+
+
+	//Approach 2 --> O(n) time complexity 
+	public static boolean checkBalanced_2(BinaryTreeNode<Integer> root){
+		return checkBalanced_help(root).isbalanced;
+	}
+	public static Pair checkBalanced_help(BinaryTreeNode<Integer>root){
+		//Base Case
+		if(root == null){
+			Pair p  = new Pair();
+			p.isbalanced = true;
+			p.height = 0;
+			return p;
+		}
+		Pair p1 = checkBalanced_help(root.left);
+		Pair ans = new Pair();
+		if(p1.isbalanced){
+			Pair p2 = checkBalanced_help(root.right);
+			if(p2.isbalanced){
+				if(Math.abs(p1.height - p2.height) <=1){
+					ans.isbalanced = true;
+					ans.height = 1 + Math.max(p1.height,p2.height);
+				}else
+					ans.isbalanced  = false;			
+			}else
+				ans.isbalanced = false;
+		}else
+			ans.isbalanced = false;
+
+		return ans; 
+	}
 	public static void main(String[]args){
 		BinaryTreeUse tree = new BinaryTreeUse();
 		BinaryTreeNode<Integer> root = tree.takeInputLevelWise();
-		System.out.println(checkBalanced(root));
+		System.out.println(checkBalanced_2(root));
 	}
+	
 }
