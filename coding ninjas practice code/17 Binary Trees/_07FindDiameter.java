8efaa51252f33bf9268480ff8ef8bc0d8e3f66a2
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-30 13:26:51 +0530

    Approach 2 : O(n) using object return type

--- coding ninjas practice code/17 Binary Trees/_07FindDiameter.java
+++ coding ninjas practice code/17 Binary Trees/_07FindDiameter.java
@@ -1,3 +1,7 @@
+class DiameterReturnType<T,V>{
+	T height;
+	V diameter;
+}
 public class _07FindDiameter{
 	//Approach 1 https://www.youtube.com/watch?v=rrXAISjtbAA --> defination in this approach doesn't include the leaf node inclusive
 	//Time Complexity is O(n*h) where h is height of tree
@@ -19,10 +23,34 @@ public class _07FindDiameter{
      		return 0;
      	return 1 + Math.max(height(root.left),height(root.right));
      }
+
+
+    //Approch 2 O(n) we will return both height and diameter by using the object of pair class
+     public static int diameterOfBinaryTree_2(BinaryTreeNode<Integer> root){
+     	return diameterOfBinaryTreeHelp(root).diameter;
+     }
+     public static DiameterReturnType<Integer,Integer>diameterOfBinaryTreeHelp(BinaryTreeNode<Integer> root){
+     	DiameterReturnType<Integer,Integer> ans = new DiameterReturnType<>();
+     	//Base Case
+     	if(root == null){
+     		ans.height = 0; 
+     		ans.diameter = 0;
+     		return ans;
+     	}
+     	DiameterReturnType<Integer,Integer> leftAns = diameterOfBinaryTreeHelp(root.left);
+     	DiameterReturnType<Integer,Integer> rightAns = diameterOfBinaryTreeHelp(root.right);
+		int option1 = 1 + leftAns.height + rightAns.height;
+     	int option2 = leftAns.diameter;
+     	int option3 = rightAns.diameter;
+
+  		ans.height = 1 +Math.max(leftAns.height,rightAns.height);
+  		ans.diameter = Math.max(option1,Math.max(option2,option3));
+  		return ans;
+     }
 	public static void main(String[]args){
 		BinaryTreeUse bt = new BinaryTreeUse();
 		BinaryTreeNode<Integer> root = bt.takeInputLevelWise();
-		System.out.println(diameterOfBinaryTree(root));
+		System.out.println(diameterOfBinaryTree_2(root));
 
 	}
 }
