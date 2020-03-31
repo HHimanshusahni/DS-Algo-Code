cfbf62059044a05a59186b0edb4cf3f6a1a854e7
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-03-31 09:36:52 +0530

    Two stack approach for printing binary tree in zig-zag manner

--- coding ninjas practice code/17 Binary Trees/_13ZigZagTree.java
+++ coding ninjas practice code/17 Binary Trees/_13ZigZagTree.java
@@ -1,38 +1,40 @@
 import java.util.Queue;
-import java.util.LinkedList;
+import java.util.Stack;
 public class _13ZigZagTree{
+	//Approach 1 Using Two stacks 
 	public  static void printZigZag(BinaryTreeNode<Integer> root) {
 		if(root == null)
-			return ;
-		boolean flag = true;
-		Queue<BinaryTreeNode<Integer>> q = new LinkedList<>();
-		q.offer(root);
-		q.offer(null);
-		while(true){
-			BinaryTreeNode<Integer> node = q.poll();
-			if(node == null){
-				if(q.isEmpty())
-					break;
-				flag = !flag;
-				System.out.println();
-				q.offer(null);
-			}else{
-				System.out.print(node.data+" ");
-				if(flag){ 
-					if(node.right != null)
-					    q.offer(node.right);
-					if(node.left != null)
-						q.offer(node.left);
-				}else{
-					if(node.left != null)
-						q.offer(node.left);
-					if(node.right != null)
-						q.offer(node.right);
-				}
+			return;
+		Stack<BinaryTreeNode<Integer>> s1 = new Stack<BinaryTreeNode<Integer>>();
+		Stack<BinaryTreeNode<Integer>> s2 = new Stack<BinaryTreeNode<Integer>>();
+		BinaryTreeNode<Integer> current = null;
+		s1.push(root);
+		while(!(s1.isEmpty() && s2.isEmpty())){
+			while(!s1.isEmpty()){
+				current = s1.pop();
+				System.out.print(current.data+" ");
+				if(current.left != null)  //Pushing elements in the left to right manner in the s2 stack
+					s2.push(current.left);
+				if(current.right != null)
+					s2.push(current.right);
+				if(s1.isEmpty())
+					System.out.println();
+			}
+			while(!s2.isEmpty()){
+				current = s2.pop();
+				System.out.print(current.data+" ");
+				if(current.right != null) //Pushing elements in the right to left manner in the s1 stack
+					s1.push(current.right);
+
+				if(current.left != null)  
+					s1.push(current.left);
+				if(s2.isEmpty())
+					System.out.println();
 			}
 		}
 
 	}	
+	
 	public static void main(String[]args){
 		BinaryTreeUse tree = new BinaryTreeUse();
 		BinaryTreeNode<Integer>root  = tree.takeInputLevelWise();
