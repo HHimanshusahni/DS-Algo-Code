aa2618bcc49e0224f160a6f610f5eb7b09b07f47
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-01 16:10:14 +0530

    Approac2 :  O(n)  for converting BST To linkedlist

--- coding ninjas practice code/18 Binary Search Tree/_03BSTToLL.java
+++ coding ninjas practice code/18 Binary Search Tree/_03BSTToLL.java
@@ -5,8 +5,12 @@ class LinkedListNode<T>{
 		this.data = data;
 	}
 }
+class Pair{
+	LinkedListNode<Integer> head;
+	LinkedListNode<Integer> tail;
+}
 public class _03BSTToLL{
-	//Approach 1
+	//Approach 1 Time Complexity O(n * h) : h is height  of the tree
 	public static LinkedListNode<Integer> BSTToSortedLL(BinaryTreeNode<Integer> root){
 		if(root == null)
 			return null;
@@ -28,9 +32,41 @@ public class _03BSTToLL{
 			return current;
 		}
 	}
+
+	//Approach 2 O(n) by returning the head of linkedlist and the tail in the recursive call
+	public static LinkedListNode<Integer> BSTToSortedLL_better(BinaryTreeNode<Integer> root){
+		Pair pair= BSTToSortedLL_betterHelper(root);
+		return pair != null ? pair.head : null; 
+	}
+	public static Pair BSTToSortedLL_betterHelper(BinaryTreeNode<Integer> root){
+		//Base Case
+		if(root == null)
+		    return null;
+		Pair ans = new Pair();
+		LinkedListNode<Integer> current = new LinkedListNode<>(root.data);
+		current.next = null;
+		Pair left = BSTToSortedLL_betterHelper(root.left);
+		Pair right = BSTToSortedLL_betterHelper(root.right);
+		if(left != null){
+			ans.head = left.head;
+			left.tail.next = current;
+		}
+		else{
+			ans.head = current;
+
+		}
+		if(right != null ){
+			ans.tail = right.tail ;
+			current.next = right.head;
+		}else{
+			ans.tail = current;
+		}
+
+		return ans;
+	}
 	public static void main(String[]args){
 		BinaryTreeNode<Integer> root = _00BinaryTreeUse.takeInputLevelWise();
-		LinkedListNode<Integer> head = BSTToSortedLL(root), temp = null;
+		LinkedListNode<Integer> head = BSTToSortedLL_better(root), temp = null;
 		temp = head;
 
 		while(temp != null){
