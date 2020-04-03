e2e283a536be0d9e9fc1eb233bbf0d7db0453df8
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-03 21:12:53 +0530

    Largest BST in Binary Tree Approach 2 : O(n)

--- coding ninjas practice code/18 Binary Search Tree/_12LargestBST.java
+++ coding ninjas practice code/18 Binary Search Tree/_12LargestBST.java
@@ -1,5 +1,11 @@
+class Value{
+	int min;
+	int max;
+	boolean present;
+	int h;
+}
 public class _12LargestBST{
-	//Approach 1  O(n^2) approach
+	//Approach 1  
 	public static int largestBSTSubtree(BinaryTreeNode<Integer> root) {
 		//Base Case
 		if(root == null)
@@ -27,9 +33,41 @@ public class _12LargestBST{
 		return 1 + Math.max(height(root.left),height(root.right));
 	}
 
+	//Approach 2
+	public static int largestBSTSubtree_2(BinaryTreeNode<Integer> root) {
+		return largestBSTSubtree_help(root).h;
+	}
+	public static Value largestBSTSubtree_help(BinaryTreeNode<Integer>root){
+		Value v = new Value();
+		if(root == null){
+			v.h = 0;
+			v.present = true;
+			v.min = Integer.MAX_VALUE;
+			v.max = Integer.MIN_VALUE; 
+			return v;
+		}
+		//Rec Case
+		Value vLeft = largestBSTSubtree_help(root.left);
+		Value vRight = largestBSTSubtree_help(root.right);
+		//Small Calculation
+		v.h = Math.max(vLeft.h,vRight.h);
+		v.min = Math.min(root.data,Math.min(vLeft.min,vRight.min));
+	    v.max = Math.max(root.data,Math.max(vLeft.max,vRight.max));
+		if(vLeft.present && vRight.present){
+			v.present = true;
+			if(root.data > vLeft.max  && root.data < vRight.min){
+				v.h ++;
+				v.present = true;
+			}else
+				v.present = false;		
+		    return v;
+		}
+		v.present = false;
+		return v;
 
+	}
 	public static void main(String[]args){
 		BinaryTreeNode<Integer> root = _00BinaryTreeUse.takeInputLevelWise();
-		System.out.println(largestBSTSubtree(root));
+		System.out.println(largestBSTSubtree_2(root));
 	}
 }
