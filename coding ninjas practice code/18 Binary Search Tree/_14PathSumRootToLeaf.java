a45b3bc872b2491e9d51247318e890a5484a19e2
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-04 10:49:39 +0530

    Approach2 Path sum root to leaf using arraylist

--- coding ninjas practice code/18 Binary Search Tree/_14PathSumRootToLeaf.java
+++ coding ninjas practice code/18 Binary Search Tree/_14PathSumRootToLeaf.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 public class _14PathSumRootToLeaf{
 	public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k) {
 		rootToLeafPathsSumToK(root,k,0,"");
@@ -19,10 +20,37 @@ public class _14PathSumRootToLeaf{
 		 	rootToLeafPathsSumToK(root.right,k,sum,path);
 
 	}
+	// Approach 2
+	public static void rootToLeafPathsSumToK_2(BinaryTreeNode<Integer>root,int k ){
+		ArrayList <Integer> list = new ArrayList<>();
+		rootToLeafPathsSumToK_2(root,k,0,list);
+	}
+	public static void rootToLeafPathsSumToK_2(BinaryTreeNode<Integer>root,int k,int sum,ArrayList<Integer> list){
+		if(root == null)
+			return;
+		sum += root.data;
+		list.add(root.data);
+		if(root.left == null && root.right == null){
+			if(sum == k ){
+				for(Integer i :list)
+					System.out.print(i+" ");
+				System.out.println();
+			}
+		    list.remove(list.size()-1);
+			return;
+		}
+		 if(root.left != null){
+			rootToLeafPathsSumToK_2(root.left,k,sum,list);
+		}
+		 if(root.right != null){
+			rootToLeafPathsSumToK_2(root.right,k,sum,list);
+		}
+		list.remove(list.size()-1);
+	}
 	public static void main(String[]args) throws IOException{
 		BinaryTreeNode<Integer> root = _00BinaryTreeUse.takeInputLevelWise();
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			int k = Integer.parseInt(br.readLine());
-			rootToLeafPathsSumToK(root,k);
+			rootToLeafPathsSumToK_2(root,k);
 	}
 }
