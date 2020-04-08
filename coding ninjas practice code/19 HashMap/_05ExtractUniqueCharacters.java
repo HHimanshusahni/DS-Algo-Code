65a88e97c943db85c2e18b7a02324ca711ebb98d
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-08 12:52:12 +0530

    Approach 2 : O(n) time and O(n) space HashSet Approach

--- coding ninjas practice code/19 HashMap/_05ExtractUniqueCharacters.java
+++ coding ninjas practice code/19 HashMap/_05ExtractUniqueCharacters.java
@@ -18,9 +18,24 @@ public class _05ExtractUniqueCharacters{
 		}
 		return ans;
 	}
+
+	//Approach 2  time : O(n) space : O(n) HashSet Approach
+	public static String uniqueChar_2(String str){
+		HashSet<Character> set = new HashSet<>();
+		String ans = "";
+		char c ;
+		for(int i = 0;i<str.length();i++){
+			c = str.charAt(i);
+			if(!set.contains(c)){
+				ans = ans+c;
+				set.add(c);
+			}
+		}
+		return ans;
+	}
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		System.out.println(uniqueChar(str));
+		System.out.println(uniqueChar_2(str));
 	}
 }
