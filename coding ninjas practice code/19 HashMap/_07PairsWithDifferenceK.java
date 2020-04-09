a5aa568242c9f32c8584b89da2237b66e42166fb
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-09 19:43:11 +0530

    Approach 2 : time : O(nlogn) space : O(1) sorting approach

--- coding ninjas practice code/19 HashMap/_07PairsWithDifferenceK.java
+++ coding ninjas practice code/19 HashMap/_07PairsWithDifferenceK.java
@@ -11,6 +11,49 @@ public class _07PairsWithDifferenceK{
 			}
 		}
 	}
+	//Approach 2 O(nlog n ) sorting approach
+	public static void findPairsDifferenceK_2(int[] input, int k){
+		Arrays.sort(input);
+		int i = 0,j = i+1;
+		int countFirst ,countSecond , count ,counter;
+		while(j<input.length){
+			if((input[j] - input[i]) == k){
+				if(input[j] == input[i]){
+					counter = 2;
+					while(j+1<input.length && input[j] == input[j+1]){
+						j++;
+						counter++;
+					}
+					i = j -1;
+					count = ( counter * (counter - 1))/2;
+
+				}else{
+					countFirst = 1;
+					countSecond = 1;
+					while(i+1<input.length && input[i] == input[i+1]){
+						i++;
+						countFirst++;
+					}
+					while(j+1 <input.length &&input[j] == input[j+1]){
+						j++;
+						countSecond++;
+					}
+					count = countFirst * countSecond;
+				}
+				for(int l = 0;l<count;l++)
+					System.out.println(input[i] +" "+input[j]);
+				i++;
+				j++;
+			}else if((input[j]- input[i]) > k){
+				i++;
+				if(i == j)
+					j++;
+			}else {
+				j++;
+			}
+		}
+	}
+
 	public static void main(String[]args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
@@ -19,6 +62,6 @@ public class _07PairsWithDifferenceK{
 		for(int i = 0;i<n;i++)
 			input[i] = Integer.parseInt(strArr[i]);
 		int k = Integer.parseInt(br.readLine());
-		findPairsDifferenceK(input,k);
+		findPairsDifferenceK_2(input,k);
 	}
 }
