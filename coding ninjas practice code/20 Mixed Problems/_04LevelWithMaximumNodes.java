9465177bb57178b7e177fc769388485c54b4235d
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-15 23:50:19 +0530

    Error Resolved for the LevelWiseMaximum Nodes current level will start from 1 and not from 0

--- coding ninjas practice code/20 Mixed Problems/_04LevelWithMaximumNodes.java
+++ coding ninjas practice code/20 Mixed Problems/_04LevelWithMaximumNodes.java
@@ -2,25 +2,26 @@ import java.util.*;
 import java.io.*;
 public class _04LevelWithMaximumNodes{
 	
-	public static int maxLevel(TreeNode<Integer> root){
+	 public static int maxLevel(TreeNode<Integer> root){
 		Queue<TreeNode<Integer>>  queue = new LinkedList<>();
 		queue.offer(root);
 		queue.offer(null);
 		int maxLevelNodes = 1;
-		int maxLevel = 0,currentLevel = 0 ,currentLevelNodes = 0;
+		int maxLevel = 0,currentLevel = 1 ,currentLevelNodes = 0;
 		while(true){
 			TreeNode<Integer> currentNode = queue.poll();
 			if(currentNode == null){
 				if(currentLevelNodes > maxLevelNodes){
-					currentLevel++;
 					maxLevel = currentLevel ;
 					maxLevelNodes = currentLevelNodes;
 				
-					currentLevelNodes = 0;
 				}
 				if(queue.isEmpty()){
 					break;
+                    
 				}
+                currentLevel++;
+				currentLevelNodes = 0;
 				queue.offer(null);
 			}else{
 				for(int i = 0; i< currentNode.children.size();i++){
