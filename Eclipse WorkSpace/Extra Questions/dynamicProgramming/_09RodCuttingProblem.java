8f0358caae8d55fe4cb00c32ac49599445a254b1
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-25 11:21:45 +0530

    Rod Cutting problem bottom up appraoch  without using extra length array

--- Eclipse WorkSpace/Extra Questions/dynamicProgramming/_09RodCuttingProblem.java
+++ Eclipse WorkSpace/Extra Questions/dynamicProgramming/_09RodCuttingProblem.java
@@ -6,7 +6,7 @@ import java.io.*;
  * but just asked in different way
  */
 public class _09RodCuttingProblem {
-	//Bottom up
+	//Bottom up 
 	public static int maxProfit(int price[],int rodLength){
 		int length[] = new int[price.length];
 		for(int i = 0 ;i < price.length; i++)
@@ -28,6 +28,20 @@ public class _09RodCuttingProblem {
 		}
 		return dp[price.length][rodLength];
 	}
+	//Bottom up 2 without using extra length array
+		public static int maxProfit_2(int price[],int rodLength){
+			
+			int dp[][] = new int[price.length+1][rodLength + 1];
+			for(int i = 1 ;i < dp.length ;i++ ){
+				for(int j = 1 ;j< dp[0].length;j++){
+					if(i <= j)
+						dp[i][j] = Math.max(price[i-1]+ dp[i][j- i],dp[i-1][j]);
+					else
+						dp[i][j] = dp[i-1][j];
+				}
+			}
+			return dp[price.length][rodLength];
+		}
 	public static void main(String[]args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testCases = Integer.parseInt( br.readLine());
@@ -37,7 +51,7 @@ public class _09RodCuttingProblem {
 			String strArr[] = br.readLine().split(" ");
 			for(int i = 0 ;i<n;i++)
 				price[i] = Integer.parseInt(strArr[i]);
-			System.out.println(	maxProfit(price,n));
+			System.out.println(	maxProfit_2(price,n));
 		
 		}
 	}
