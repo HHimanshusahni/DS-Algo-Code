22e40cb2dc9364a3205c3374cdef8702ca916412
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-27 13:44:07 +0530

    LCS: Memoization Approach

--- Eclipse WorkSpace/Extra Questions/dynamicProgramming/_12LongestCommonSubsequence.java
+++ Eclipse WorkSpace/Extra Questions/dynamicProgramming/_12LongestCommonSubsequence.java
@@ -1,5 +1,7 @@
 package dynamicProgramming;
 import java.io.*;
+import java.util.Arrays;
+//https://practice.geeksforgeeks.org/problems/longest-common-subsequence/0
 public class _12LongestCommonSubsequence {
 	//Recursive Approach
 	public static int LCS(String X, String Y, int n , int m ){
@@ -14,6 +16,31 @@ public class _12LongestCommonSubsequence {
 			return Math.max(LCS(X,Y,n-1,m),LCS(X,Y,n,m-1));
 		
 	}
+	
+	//Memoization Approach
+	static int dp[][] = new int[101][101];//Intialize size based on the question
+	public static int LCS_Mem(String X, String Y, int n , int m){
+		for(int i = 0 ;i< n + 1 ;i++){
+			Arrays.fill(dp[i], 0,m +1,-1);
+		}
+		return LCS_2(X,Y,n,m);
+	}
+	public static int LCS_2(String X, String Y, int n , int m ){
+		//Base Case
+		if(n == 0 || m == 0)
+			return 0;
+		//Check the dp matrix
+		if(dp[n][m] != -1)
+			return dp[n][m];
+		
+		//Recursive Case from Choice diagram
+		//If the characters of the strings match , then we include in the LCS
+		if(X.charAt(n-1)== Y.charAt(m-1))
+			return dp[n][m] = 1 + LCS_2(X,Y ,n-1,m - 1);
+		else //If the characters of the string don't match
+			return dp[n][m] = Math.max(LCS_2(X,Y,n-1,m),LCS_2(X,Y,n,m-1));
+		
+	}
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testCases = Integer.parseInt(br.readLine());
@@ -23,7 +50,7 @@ public class _12LongestCommonSubsequence {
 			int m = Integer.parseInt(strArr[1]);
 			String X = br.readLine();
 			String Y = br.readLine(); 
-			System.out.println(LCS(X,Y,n,m));
+			System.out.println(LCS_Mem(X,Y,n,m));
 		}
 	
 	}
