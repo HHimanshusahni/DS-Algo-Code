d8db488e910d22d9e78ef2b2f32fd18f2c6b7c9e
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-04-28 22:22:30 +0530

    Bottom up approach : LCS Oorrection

--- Eclipse WorkSpace/Extra Questions/dynamicProgramming/_12LongestCommonSubsequence.java
+++ Eclipse WorkSpace/Extra Questions/dynamicProgramming/_12LongestCommonSubsequence.java
@@ -52,7 +52,7 @@ public class _12LongestCommonSubsequence {
 			dp[j][0] = 0;
 		for(int i = 1; i < n+1;i++){
 			for(int j = 1; j< m+1;j++){
-				if(X.charAt(i)== Y.charAt(j))
+				if(X.charAt(i-1)== Y.charAt(j-1))
 					dp[i][j] = 1 + dp[i-1][j-1];
 				else
 					dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);	
