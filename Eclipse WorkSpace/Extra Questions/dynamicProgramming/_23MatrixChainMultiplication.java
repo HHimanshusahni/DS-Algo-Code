5e66852d58019da0606a7f603642dfc76a279b5c
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-05-09 14:52:01 +0530

    MCM Memoization Approach

--- Eclipse WorkSpace/Extra Questions/dynamicProgramming/_23MatrixChainMultiplication.java
+++ Eclipse WorkSpace/Extra Questions/dynamicProgramming/_23MatrixChainMultiplication.java
@@ -1,8 +1,10 @@
 package dynamicProgramming;
 import java.io.*;
+import java.util.Arrays;
 //https://www.geeksforgeeks.org/matrix-chain-multiplication-dp-8/
 //https://practice.geeksforgeeks.org/problems/matrix-chain-multiplication/0
 public class _23MatrixChainMultiplication {
+	//Recursive Solution
 	public static int mcm(int arr[],int i , int j ){
 		//Base Case
 		if(i >= j ) //When there is 1 element no  need to mutliply
@@ -17,6 +19,31 @@ public class _23MatrixChainMultiplication {
 		}
 		return minAns;
 	}
+	//Memoization
+	static int dp[][] = new int[101][101];
+	public static int mcm_mem(int arr[],int i , int j){
+		int n = arr.length;
+		for(int a = 0 ; a < n;a++)
+			Arrays.fill(dp[a],0,n,-1);
+		return  mcm_2(arr,i,j);
+	}
+	public static int mcm_2(int arr[],int i ,int j ){
+		//Base Case
+		if(i >= j)
+			return 0;
+		if(dp[i][j] != -1)
+			return dp[i][j];
+		int tempAns;
+		int minAns = Integer.MAX_VALUE;
+		for(int k = i ; k <= j-1; k++){
+			tempAns = mcm_2(arr , i ,k) +
+					  mcm_2(arr,k+1,j) + 
+					  arr[i-1] * arr[k] * arr[j];
+			if(tempAns < minAns)
+				minAns = tempAns;
+		}
+		return  dp[i][j] = minAns;
+	}
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int test = Integer.parseInt(br.readLine());
@@ -26,7 +53,7 @@ public class _23MatrixChainMultiplication {
 			String strArr[] = br.readLine().split(" ");
 			for(int i = 0 ;i < n;i++)
 				arr[i] = Integer.parseInt(strArr[i]);
-			System.out.println(mcm(arr,1,n-1));
+			System.out.println(mcm_mem(arr,1,n-1));
 		}
 	}
 
