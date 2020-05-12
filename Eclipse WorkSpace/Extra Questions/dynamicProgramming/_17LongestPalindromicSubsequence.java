3135ac8a236d2998a983286bcc189a8d1f8287be
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-05-12 14:19:52 +0530

    Approach 2 : Bottom Up  LPS

--- Eclipse WorkSpace/Extra Questions/dynamicProgramming/_17LongestPalindromicSubsequence.java
+++ Eclipse WorkSpace/Extra Questions/dynamicProgramming/_17LongestPalindromicSubsequence.java
@@ -30,11 +30,39 @@ public class _17LongestPalindromicSubsequence {
 		}
 		return dp[m];
 	} 
+	
+	
+	//Approach 2 
+	/*Match the lps ( i , j ) = characters match => 2 + lps(i+1, j - 1)
+	 *						  = characters don't match = Max( lps(i, j-1),lps(i+1,j))
+	 *							= 1 if ( i == j) //Base Case
+	 * */					  
+	//Time Complexity bottom up O(n ^2) Space complexity :O(n^2)
+	//https://www.geeksforgeeks.org/longest-palindromic-subsequence-dp-12/
+	//https://www.youtube.com/watch?v=U4yPae3GEO0
+
+	public static int lps_2(String s ){
+		int n = s.length();
+		int dp[][] = new int[n][n];
+		//One length strings is always palindrome
+		for(int i = 1 ;i < n ;i++)
+			dp[i][i] = 1;
+		for(int length = 2 ;length <= n ;length++){
+			for(int i = 0 ;i < n - length + 1 ;i++){
+				int j = i + length -1 ;
+				if(s.charAt(i) == s.charAt(j))
+					dp[i][j] = 2 + dp[i+1][j-1];
+				else 
+					dp[i][j] = Math.max (dp[i][j-1], dp[i+1][j]);
+			}	
+		}
+		return dp[0][n-1];
+	}
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testCase = Integer.parseInt(br.readLine());
 		for(int t = 0 ; t < testCase; t++){
-			System.out.println(lps(br.readLine()));
+			System.out.println(lps_2(br.readLine()));
 		}		
 	}
 
