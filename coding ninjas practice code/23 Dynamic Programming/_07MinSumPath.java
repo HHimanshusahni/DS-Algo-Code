0a2139f21e59f9411fd1fb846abb4b2161c20d89
HHimanshusahni <sahnihimanshu97@gmail.com>
2020-05-22 20:32:13 +0530

    Bottom up 1d dp arraysolution for  minSumPathCost

--- coding ninjas practice code/23 Dynamic Programming/_07MinSumPath.java
+++ coding ninjas practice code/23 Dynamic Programming/_07MinSumPath.java
@@ -66,6 +66,31 @@ public class _07MinSumPath{
 		}  
 		return dp[0][0];
 	}
+	//Bottom Up Approach 1d dp array T : O(input size) S:(row length of matrix input)
+	//At any moment while filling the dp table we are using the value at the lower column [i+1][j],right value[i][j+1],diagonal value[i+1][j+1]
+	public static int minCostPath_4(int input[][]){
+		int dp[] = new int[input[0].length];
+		int rightLowerDiagonal;
+		int temp;
+		for(int i = input.length - 1; i >= 0 ;i-- ){
+			rightLowerDiagonal = Integer.MAX_VALUE;
+			for(int j = input[0].length -1 ; j >= 0 ;j--){
+				temp = dp[j]; 
+				if(i == input.length - 1 && j == input[0].length - 1)	//For the last row last column ie desitnation cell we need to just take input value
+					dp[j] = input[i][j];
+				else if(i == input.length - 1 )//For the last row we need to consider right value only
+					dp[j] = input[i][j] + dp[j+1];
+				else if(j == input[0].length - 1)//For the last column we need to consider the below value only
+					dp[j] = input[i][j] + dp[j];
+				else {
+					dp[j] = input[i][j] + Math.min(rightLowerDiagonal,Math.min(dp[j],dp[j+1]));
+				}
+				rightLowerDiagonal = temp;
+			}		
+
+		}
+		return dp[0];
+	}
 	public static void main(String[]args){
 		Scanner s = new Scanner(System.in);
 		int m = s.nextInt();
@@ -75,6 +100,6 @@ public class _07MinSumPath{
 			for(int j = 0 ;j < n ;j++)
 				input[i][j] = s.nextInt();
 		}
-		System.out.println(minCostPath_3(input));
+		System.out.println(minCostPath_4(input));
 	}
 }
